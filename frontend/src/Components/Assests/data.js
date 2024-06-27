c97029ecb7f4ecaf22f975a91253be8f7c6b1400
rajeshvelusamyV <rajeshvelusamy17042003@gmail.com>
2024-06-27 19:20:44 +0530

    Initial commit with Ecommerce project files

---
+++ frontend/src/Components/Assests/data.js
@@ -0,0 +1,39 @@
+import p1_img from './product1.jpg';
+import p2_img from './product2.jpg';
+import p3_img from './product3.webp';
+import p4_img from './product4.jpg';
+import p5_img from './product5.webp';
+import p6_img from './product6.jpg';
+import p7_img from'./product7.jpg';
+let data_product=[
+    // {
+    //     id:1,
+    //     name:"Stripped Sleeve Blouse",
+    //     image:p1_img,
+    //     new_price:50.00,
+    //     old_price:80.50,
+    // },
+    {
+        id:1,
+        name:"gown",
+        image:p2_img,
+        new_price:659.00,
+        old_price:999.00,
+
+    },
+    {
+        id:2,
+        name:"chudidhar",
+        image:p7_img,
+        new_price:446.00,
+        old_price:667.00,
+    },
+    {
+        id:3,
+        name:"saree",
+        image:p6_img,
+        new_price:1110.00,
+        old_price:1220.00,
+    }
+]
+export default data_product;
